0dddeb4385a7b296e297196bdee03226c9a91184
Nishantth1 <97899868+Nishantth1@users.noreply.github.com>
2024-05-20 11:34:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 from resources.user import UserResource
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
 
 api = Api(app)
+
+# Define the root resource
+class RootResource(Resource):
+    def get(self):
+        return jsonify({"message": "Welcome to the API"})
+
+# Add resources to the API
+api.add_resource(RootResource, '/')
 api.add_resource(UserResource, '/users', '/users/<string:user_id>')
 
 if __name__ == '__main__':
